9d3e7f48ac06444facd919fd23a1f236e404024b
jonac19 <jonac19@uci.edu>
2022-10-05 12:53:10 -0700

    Added MySQL information retrieval and basic display

--- src/MovieListServlet.java
+++ src/MovieListServlet.java
@@ -1,4 +1,98 @@
-package src;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
-public class MovieListServlet {
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+// Declaring a WebServlet called MovieListServlet, which maps to url "/api/movieList"
+@WebServlet(name = "MovieListServlet", urlPatterns = "/api/movieList")
+public class MovieListServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    // Create a dataSource which registered in web.
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/cs122b");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType("application/json"); // Response mime type
+
+        // Output stream to STDOUT
+        PrintWriter out = response.getWriter();
+
+        // Get a connection from dataSource and let resource manager close the connection after usage.
+        try (Connection conn = dataSource.getConnection()) {
+
+            // Declare our statement
+            Statement statement = conn.createStatement();
+
+            String query = "SELECT * FROM movies";
+
+            // Perform the query
+            ResultSet rs = statement.executeQuery(query);
+
+            JsonArray jsonArray = new JsonArray();
+
+            // Iterate through each row of rs
+            while (rs.next()) {
+                String movie_id = rs.getString("id");
+                String movie_title = rs.getString("title");
+                String movie_year = rs.getString("year");
+                String movie_director = rs.getString("director");
+
+                // Create a JsonObject based on the data we retrieve from rs
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("movie_id", movie_id);
+                jsonObject.addProperty("movie_title", movie_title);
+                jsonObject.addProperty("movie_year", movie_year);
+                jsonObject.addProperty("movie_director", movie_director);
+
+                jsonArray.add(jsonObject);
+            }
+            rs.close();
+            statement.close();
+
+            // Log to localhost log
+            request.getServletContext().log("getting " + jsonArray.size() + " results");
+
+            // Write JSON string to output
+            out.write(jsonArray.toString());
+            // Set response status to 200 (OK)
+            response.setStatus(200);
+
+        } catch (Exception e) {
+
+            // Write error message JSON object to output
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            // Set response status to 500 (Internal Server Error)
+            response.setStatus(500);
+        } finally {
+            out.close();
+        }
+    }
 }
---
+++ web/index.js
@@ -0,0 +1,35 @@
+/**
+ * Handles the data returned by the API, read the jsonObject and populate data into html elements
+ * @param resultData jsonObject
+ */
+function handleMovieListResult(resultData) {
+    console.log("handleMovieListResult: populating movie list table from resultData");
+
+    // Populate the movie list table
+    // Find the empty table body by id "movie_list_table_body"
+    let movieListTableBodyElement = jQuery("#movie_list_table_body");
+
+    // Iterate through resultData, no more than 10 entries
+    for (let i = 0; i < Math.min(20, resultData.length); i++) {
+        // Concatenate the html tags with resultData jsonObject
+        let rowHTML = "";
+        rowHTML += "<tr>"
+        rowHTML += "<th>" + resultData[i]["movie_title"] + "</th>";
+        rowHTML += "<th>" + resultData[i]["movie_year"] + "</th>";
+        rowHTML += "<th>" + resultData[i]["movie_director"] + "</th>";
+        rowHTML += "<th>" + "Tempory" + "</th>";
+        rowHTML += "<th>" + "Tempory" + "</th>";
+        rowHTML += "<th>" + "Tempory" + "</th>";
+        rowHTML += "</tr>";
+
+        // Append the row created to the table body, which will refresh the page
+        movieListTableBodyElement.append(rowHTML);
+    }
+}
+
+jQuery.ajax({
+    dataType: "json",
+    method: "GET",
+    url: "api/movieList",
+    success: (resultData) => handleMovieListResult(resultData)
+});
