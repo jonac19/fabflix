7e9aa73df42842f4b2683c5c9e1e074bb013a644
jonac19 <jonac19@uci.edu>
2022-10-09 20:41:12 -0700

    Updated MySQL database to correct name

--- src/StarServlet.java
+++ src/StarServlet.java
@@ -25,7 +25,7 @@ public class StarServlet extends HttpServlet {
 
     public void init(ServletConfig config) {
         try {
-            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/cs122b");
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
         } catch (NamingException e) {
             e.printStackTrace();
         }
