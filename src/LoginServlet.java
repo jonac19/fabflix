4a6f93991aea3da1db814849d8c44a9d30473150
jonac19 <jonac19@uci.edu>
2022-10-17 21:15:19 -0700

    Login page uses database to verify customers

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -1,44 +1,114 @@
 import com.google.gson.JsonObject;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/api/login")
 public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 4L;
+
+    // Create a dataSource which registered in web.
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
     /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        response.setContentType("application/json"); // Response mime type
+
+        // Output stream to STDOUT
+        PrintWriter out = response.getWriter();
+
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-        /* This example only allows username/password to be test/test
-        /  in the real project, you should talk to the database to verify username/password
-        */
         JsonObject responseJsonObject = new JsonObject();
-        if (username.equals("anteater") && password.equals("123456")) {
-            // Login success:
+        try (Connection conn = dataSource.getConnection()) {
+            String query = "SELECT * FROM customers C WHERE C.email = ?";
 
-            // set this user into the session
-            request.getSession().setAttribute("user", new User(username));
+            // Declare our statement
+            PreparedStatement statement = conn.prepareStatement(query);
 
-            responseJsonObject.addProperty("status", "success");
-            responseJsonObject.addProperty("message", "success");
+            // Set the parameter represented by "?" in the query to the id we get from url,
+            // num 1 indicates the first "?" in the query
+            statement.setString(1, username);
 
-        } else {
-            // Login fail
-            responseJsonObject.addProperty("status", "fail");
-            // Log to localhost log
-            request.getServletContext().log("Login failed");
-            // sample error messages. in practice, it is not a good idea to tell user which one is incorrect/not exist.
-            if (!username.equals("anteater")) {
+            // Perform the query
+            ResultSet rs = statement.executeQuery();
+
+            if (!rs.isBeforeFirst()) {
+                // Login fail due to incorrect username
+                responseJsonObject.addProperty("status", "fail");
+                // Log to localhost log
+                request.getServletContext().log("Login failed");
                 responseJsonObject.addProperty("message", "user " + username + " doesn't exist");
             } else {
-                responseJsonObject.addProperty("message", "incorrect password");
+                boolean passwordMatched = false;
+
+                while (rs.next()) {
+                    if (password.equals(rs.getString("password"))) {
+                        // Login success:
+
+                        // set this user into the session
+                        request.getSession().setAttribute("user", new User(username));
+
+                        responseJsonObject.addProperty("status", "success");
+                        responseJsonObject.addProperty("message", "success");
+
+                        passwordMatched = true;
+                        break;
+                    }
+                }
+
+                if (!passwordMatched) {
+                    // Login fail due to incorrect password
+                    responseJsonObject.addProperty("status", "fail");
+                    // Log to localhost log
+                    request.getServletContext().log("Login failed");
+                    responseJsonObject.addProperty("message", "incorrect password");
+                }
             }
+            rs.close();
+            statement.close();
+
+            // Write JSON string to output
+            out.write(responseJsonObject.toString());
+            // Set response status to 200 (OK)
+            response.setStatus(200);
+        } catch (Exception e) {
+            // Write error message JSON object to output
+            // Login fail
+            responseJsonObject.addProperty("status", "fail");
+            responseJsonObject.addProperty("message", e.getMessage());
+            // Log to localhost log
+            request.getServletContext().log("Login failed");
+
+            out.write(responseJsonObject.toString());
+
+            // Set response status to 500 (Internal Server Error)
+            response.setStatus(500);
+        } finally {
+            out.close();
         }
-        response.getWriter().write(responseJsonObject.toString());
+
     }
 }
--- web/login.js
+++ web/login.js
@@ -7,18 +7,12 @@ let login_form = $("#login_form");
 function handleLoginResult(resultDataString) {
     let resultDataJson = JSON.parse(resultDataString);
 
-    console.log("handle login response");
-    console.log(resultDataJson);
-    console.log(resultDataJson["status"]);
-
     // If login succeeds, it will redirect the user to index.html
     if (resultDataJson["status"] === "success") {
         window.location.replace("index.html");
     } else {
         // If login fails, the web page will display
         // error messages on <div> with id "login_error_message"
-        console.log("show error message");
-        console.log(resultDataJson["message"]);
         $("#login_error_message").text(resultDataJson["message"]);
     }
 }
