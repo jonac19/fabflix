9e7d51b34cfd9daf9d1ad06d934b99d8985d4fc6
skonaka6 <78990481+skonaka6@users.noreply.github.com>
2022-10-23 12:44:55 -0700

    Import files

--- web/index.js
+++ web/index.js
@@ -104,6 +104,72 @@ function orderBy(column, order) {
         + "&browseTitle=" + listBrowseTitle);
 }
 
+/**
+ * Handle the data returned by IndexServlet
+ * @param resultDataString jsonObject, consists of session info
+ */
+function handleSessionData(resultDataString) {
+    let resultDataJson = JSON.parse(resultDataString);
+
+    console.log("handle session response");
+    console.log(resultDataJson);
+    console.log(resultDataJson["sessionID"]);
+
+    // show the session information
+    $("#sessionID").text("Session ID: " + resultDataJson["sessionID"]);
+    $("#lastAccessTime").text("Last access time: " + resultDataJson["lastAccessTime"]);
+
+    // show cart information
+    handleCartArray(resultDataJson["previousItems"]);
+}
+
+/**
+ * Handle the items in item list
+ * @param resultArray jsonObject, needs to be parsed to html
+ */
+function handleCartArray(resultArray) {
+    console.log(resultArray);
+    let item_list = $("#item_list");
+    // change it to html list
+    let res = "<ul>";
+    for (let i = 0; i < resultArray.length; i++) {
+        // each item will be in a bullet point
+        res += "<li>" + resultArray[i] + "</li>";
+    }
+    res += "</ul>";
+
+    // clear the old array and show the new array in the frontend
+    item_list.html("");
+    item_list.append(res);
+}
+
+/**
+ * Submit form content with POST method
+ * @param cartEvent
+ */
+function handleCartInfo(cartEvent) {
+    console.log("submit cart form");
+    /**
+     * When users click the submit button, the browser will not direct
+     * users to the url defined in HTML form. Instead, it will call this
+     * event handler when the event is triggered.
+     */
+    cartEvent.preventDefault();
+
+    $.ajax("api/index", {
+        method: "POST",
+        data: cart.serialize(),
+        success: resultDataString => {
+            let resultDataJson = JSON.parse(resultDataString);
+            handleCartArray(resultDataJson["previousItems"]);
+        }
+    });
+
+    // clear input form
+    cart[0].reset();
+}
+
+
 let searchForm = jQuery("#search_form");
 searchForm.submit(submitSearchForm);
 
@@ -117,6 +183,7 @@ let listSearchDirector = getParameterByName("searchDirector");
 let listSearchStar = getParameterByName("searchStar");
 let listBrowseGenre = getParameterByName("browseGenre");
 let listBrowseTitle = getParameterByName("browseTitle");
+let cart = $("#cart");
 
 if (listLimit == null) {listLimit = ""};
 if (listCriteria == null) {listCriteria = ""};
@@ -142,3 +209,10 @@ jQuery.ajax({
          + "&browseTitle=" + listBrowseTitle,
     success: (resultData) => handleMovieListResult(resultData)
 });
+
+$.ajax("api/index", {
+    method: "GET",
+    success: handleSessionData
+});
+
+cart.submit(handleCartInfo);
