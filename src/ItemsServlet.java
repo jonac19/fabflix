700cf6414531bdf39aa9575c93448e2110aadfe8
skonaka6 <78990481+skonaka6@users.noreply.github.com>
2022-10-23 16:05:52 -0700

    Implemented front/backend separation
    -Created items.html and .js
    -Modified ItemsServlet.java to follow MovieServlet structuring

--- src/ItemsServlet.java
+++ src/ItemsServlet.java
@@ -1,3 +1,4 @@
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import javax.naming.InitialContext;
@@ -16,8 +17,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
-// Declaring a WebServlet called ItemServlet, which maps to url "/items"
-@WebServlet(name = "ItemServlet", urlPatterns = "/items")
+// Declaring a WebServlet called ItemServlet, which maps to url "api/items"
+@WebServlet(name = "ItemServlet", urlPatterns = "api/items")
 
 public class ItemsServlet extends HttpServlet {
 
@@ -52,19 +53,8 @@ public class ItemsServlet extends HttpServlet {
 
         String newItem = request.getParameter("newItem"); // Get parameter that sent by GET request url
 
-        response.setContentType("text/html");
+        response.setContentType("application/json");
         PrintWriter out = response.getWriter();
-        String title = "Items Purchased";
-
-        out.println(String.format("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">\n" +
-                "<html>\n" +
-                "   <head>" +
-                "   <title>%s</title>" +
-                "   </head>\n" +
-                "   <body bgcolor='#000000'>\n" + //  bgcolor="#FDF5E6"
-                "       <h1 style='color:white'>%s</h1>", title, title));
-        out.println( " <h3><a href='http://localhost:8080/cs122b-fall22-team-46/'>" +
-                "Back to Main Page</a></h3>");
 
         // In order to prevent multiple clients, requests from altering previousItems ArrayList at the same time, we
         // lock the ArrayList while updating
@@ -73,39 +63,42 @@ public class ItemsServlet extends HttpServlet {
                 previousItems.add(newItem); // Add the new item to the previousItems ArrayList
             }
 
-            // Display the current previousItems ArrayList
-            if (previousItems.size() == 0) {
-                out.println("<i>No items</i>");
-            } else {
+            JsonArray jsonArray = new JsonArray();
 
-                try (Connection conn = dataSource.getConnection()){
-                    out.println("<ul style='color:black'>");
-                    for (String previousItem : previousItems) {
-                        // Call database to match previousItem (movieID) to movie title
-                        String query = "SELECT * FROM movies M WHERE M.id=?";
-                        PreparedStatement statement = conn.prepareStatement(query);
-                        statement.setString(1, previousItem);
-                        ResultSet rs = statement.executeQuery();
-                        rs.next();
-                        // Output as list-item
-                        out.println("<li style='color:white'>" + rs.getString("title"));
-                    }
-                    out.println("</ul>");
+            try (Connection conn = dataSource.getConnection()){
+                out.println("<ul style='color:black'>");
+                for (String previousItem : previousItems) {
+                    // Call database to match previousItem (movieID) to movie title
+                    String query = "SELECT * FROM movies M WHERE M.id=?";
+                    PreparedStatement statement = conn.prepareStatement(query);
+                    statement.setString(1, previousItem);
+                    ResultSet rs = statement.executeQuery();
+                    rs.next();
 
-                } catch (Exception e) {
+                    // Make JSON Object to hold movie title, add to Array, and send to items.js
+                    JsonObject jsonObject = new JsonObject();
+                    String movie_Title = rs.getString("title");
+                    jsonObject.addProperty("movie_title", movie_Title);
+                    jsonArray.add(jsonObject);
 
-                // Write error message JSON object to output
-                JsonObject jsonObject = new JsonObject();
-                jsonObject.addProperty("errorMessage", e.getMessage());
-                out.write(jsonObject.toString());
-
-                // Set response status to 500 (Internal Server Error)
-                response.setStatus(500);
+                    rs.close();
+                    statement.close();
                 }
+                out.write(jsonArray.toString());
+                response.setStatus(200);
 
+            } catch (Exception e) {
+
+            // Write error message JSON object to output
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            // Set response status to 500 (Internal Server Error)
+            response.setStatus(500);
             }
-        }
 
-        out.println("</body></html>");
+        }
+        out.close();
     }
 }
---
+++ web/items.js
@@ -0,0 +1,54 @@
+const unit_price = 4.99;
+/**
+ * Retrieve parameter from request URL, matching by parameter name
+ * @param target String
+ * @returns {*}
+ */
+function getParameterByName(target) {
+    // Get request URL
+    let url = window.location.href;
+    // Encode target parameter name to url encoding
+    target = target.replace(/[\[\]]/g, "\\$&");
+
+    // Ues regular expression to find matched parameter value
+    let regex = new RegExp("[?&]" + target + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+
+    // Return the decoded parameter value
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+/**
+ * Handles the data returned by the API, read the jsonObject and populate data into html elements
+ * @param resultData jsonObject
+ */
+function handleItemsResult(resultData) {
+    console.log("handleItemsResult: populating items table from resultData");
+
+    let itemsTableBodyElement = jQuery("#items_table_body");
+
+    for (let i = 0; i < resultData.length; i++) {
+        // Concatenate the html tags with resultData jsonObject
+        let rowHTML = "";
+        rowHTML += "<tr>"
+        rowHTML += "<td>" + resultData[i]["movie_title"] + "</td>";
+        rowHTML += "<td>" + unit_price + "</td>";
+        rowHTML += "<td>" + "quantity placeholder" + "</td>";
+        rowHTML += "<td>" + "remove placeholder" + "</td>";
+
+        // Append the row created to the table body, which will refresh the page
+        itemsTableBodyElement.append(rowHTML);
+    }
+}
+
+// Get item id (movie id) from URL
+let movieId = getParameterByName("newItem")
+
+jQuery.ajax({
+    dataType: "json",
+    method: "GET",
+    url: "api/items?newItem=" + movieId,
+    success: (resultData) => handleItemsResult(resultData)
+});
