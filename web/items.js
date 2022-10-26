7a68730b031ba76bf7e9fa1d9ef89a02df068c0d
skonaka6 <78990481+skonaka6@users.noreply.github.com>
2022-10-25 19:11:24 -0700

    Changed Shopping Cart page QTY changes and Remove to not need to reload the page

--- web/items.js
+++ web/items.js
@@ -7,6 +7,7 @@ const unit_price = 4.99;
 function handleShoppingCartResult(resultData) {
     console.log(resultData);
     let items_table_body = $("#items_table_body");
+    $("#items_table_body tr").remove(); //Reset the table
 
     let total_cost = 0;
     for (let i = 0; i < resultData.length; i++) {
@@ -20,7 +21,7 @@ function handleShoppingCartResult(resultData) {
                 res += "<td>" + movieData[0]["movie_title"] + "</td>"; //movie title
                 res += "<td>" + (Math.round(unit_price * parseInt(resultData[i]["quantity"]) *100) / 100).toString() + "</td>";
                 res += "<td>" + "<input type='number' value='" + resultData[i]["quantity"] +"' min='1' name='qty' disabled=true " +
-                    "oninput='this.value = Math.abs(this.value)' " + "</td>";
+                    "oninput='this.value = Math.abs(this.value)' id=qty'" + resultData[i]["movie_id"].toString() + "'></td>";
                 res += "<td>" + "<button onclick='handleIncrementMovie(\"" + resultData[i]["movie_id"] + "\")'>+1</button>";
                 res += "<td>" + "<button onclick='handleDecrementMovie(\"" + resultData[i]["movie_id"] + "\")'>-1</button>";
                 res += "<td>" + "<button onclick='handleRemoveMovie(\"" + resultData[i]["movie_id"] + "\")'>Remove</button>";
@@ -33,6 +34,7 @@ function handleShoppingCartResult(resultData) {
         total_cost += unit_price * parseInt(resultData[i]["quantity"]);
     }
 
+    document.getElementById("total_cost").innerText = "Total Cost: $";  // Reset total cost text
     $("#total_cost").append((Math.round(total_cost*100)/100).toString());   //Display total cost
 }
 
@@ -40,18 +42,38 @@ function handleIncrementMovie( movie_id ){
     $.ajax({
         url: "api/items?action=increment&movie_id=" + movie_id,
         method: "POST",
+        async: false
     });
 
-    document.location.reload();
+    // document.location.reload();
+    $("#qty" + movie_id).value++;
+
+    $.ajax({
+        dataType: "json",
+        method: "GET",
+        url: "api/items",
+        async: false,
+        success: (resultData) => handleShoppingCartResult(resultData)
+    });
 }
 
 function handleDecrementMovie( movie_id ){
     $.ajax({
         url: "api/items?action=decrement&movie_id=" + movie_id,
         method: "POST",
+        async: false
     });
 
-    document.location.reload();
+    // document.location.reload();
+    $("#qty" + movie_id).value++;
+
+    $.ajax({
+        dataType: "json",
+        method: "GET",
+        url: "api/items",
+        async: false,
+        success: (resultData) => handleShoppingCartResult(resultData)
+    });
 }
 
 function handleRemoveMovie( movie_id ){
@@ -60,7 +82,13 @@ function handleRemoveMovie( movie_id ){
         method: "POST",
     });
 
-    document.location.reload();
+    $.ajax({
+        dataType: "json",
+        method: "GET",
+        url: "api/items",
+        async: false,
+        success: (resultData) => handleShoppingCartResult(resultData)
+    });
 }
 
 $.ajax({
